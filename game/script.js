20e59647d51607bccb7e7cabbf0299b8c66428f2
avanalphen <vanalphena@gmail.com>
2021-06-20 21:46:54 -0400

    added Physical Attack & Spell attack

--- game/script.js
+++ game/script.js
@@ -27,6 +27,14 @@ class Character {
           return `${this.name} has died in act of combat.`;
         }
     };
+
+    physicalAttack = () => {
+        return Math.floor(Math.random() * this.strength);
+    }
+
+    spellAttack = () => {
+        return Math.floor(Math.random() * this.strength)+1;
+    }
 }
 
 class Warrior extends Character {
@@ -38,16 +46,47 @@ class Warrior extends Character {
 
 class FinalBoss extends Character {
     spell = () => {
-
+        console.log('Siphon Soul')
     }
 }
 
+class Grunt extends Character {
+    spell = () => {
+        console.log('Scream')
+    }
+ }
+
+class Dragon extends Character {
+    spell = () => {
+        console.log('Breathe Fire')
+    }
+}
+
+class IceWoman extends Character {
+    spell = () => {
+        console.log('Ice Bolt')
+    }
+} 
+
 let warrior = new Warrior('Warrior', 12, 6, 100, 2, 2);
+
+
+let mage = new Mage('Mage', 6, 12, 80, 0, 2);
+
+
+let rogue = new Rogue('Rogue', 9, 9, 90, 3, 2);
 
 
 let finalBoss = new FinalBoss('Final Boss', 25, 14, 60, 3, 2);
 
 
+let grunt = new Grunt('Grunt', 12, 5, 30, 1, 3);
+
+
+let dragon = new Dragon('Dragon', 9, 10, 30, 2, 2);
+
+
+let iceWoman = new IceWoman('Ice Woman', 5, 12, 25, 1 ,2);
 
 
 // class Mage extends Character {
