1ce7287970423e03dbac9bad9c7819131c33f168
jose fraga <josemendezfraga@gmail.com>
2021-06-22 12:50:34 -0400

    everything organized

--- battle/battle.js
+++ battle/battle.js
@@ -1,105 +1,203 @@
-// const canvas = document.querySelector('canvas')
-// canvas.width = window.innerWidth;
-// canvas.height = window.innerHeight;
+class Character {
+  constructor(name, strength, spellPower, health, spellCooldown, defenseCooldown) {
+      this.name = name,
+      this.strength = strength,
+      this.spellPower = spellPower,
+      this.spellCooldown = spellCooldown,
+      this.health = health,
+      this.defenseCooldown = defenseCooldown,
+      this.defenseUsed = false
+  }
+
+  receiveDamage = (enemy, damage) => {
+
+      this.health -= damage;
+      if (this.health > 0) {
+        return `${this.name} has received ${damage} points of damage from ${enemy.name}. Now he has a health of ${this.health}`;
+      } else {
+        return `${this.name} has died in act of combat.`;
+      }
+  };
+
+  physicalAttack = (enemy) => {
+      return enemy.receiveDamage(this, Math.floor(Math.random() * this.strength));
+  }
+
+  spellAttack = (enemy) => {
+      return enemy.receiveDamage(this, Math.floor(Math.random() * this.strength)+1);
+  }
+}
+
+class Warrior extends Character {
+
+  spell = () => {
+      console.log('Toss Axe');
+  }
+
+  receiveDamage = (enemy, damage) => {
+      if (this.defenseUsed === true){
+          this.health -= damage/2;
+          this.defenseUsed = false;
+      } else {
+          this.health -= damage;
+      }
+      if (this.health > 0) {
+        return `${this.name} has received ${damage} points of damage from ${enemy.name}. Now he has a health of ${this.health}`;
+      } else {
+        return `${this.name} has died in act of combat.`;
+      }
+  };
+
+  defense = () => {
+      this.defenseUsed = true;
+  }
+}
+
+class Mage extends Character {
+
+  spell = () => {
+      console.log('Magic Burst')
+  }
+
+  defense = () => {
+      this.health += 15;
+      return `'Mage has used Mend Wounds. Her health is now ${this.health}`
+ }
+}
+
+class Rogue extends Character {
+
+  spell = () => {
+       console.log('Shadow Step')
+   }
+
+   receiveDamage = (enemy, damage) => {
+      if (this.defenseUsed === true){
+          damage = 0;
+          this.health -= damage;
+          this.defenseUsed = false;
+      } else {
+          this.health -= damage;
+      }
+      if (this.health > 0) {
+        return `${this.name} has received ${damage} points of damage from ${enemy.name}. He now has a health of ${this.health}`;
+      } else {
+        return `${this.name} has died in act of combat.`;
+      }
+  };
 
 
-// attack variables
-attackName = '';
-curAttack = {};
-randInt = 0;
-enemyAttack = {};
-defendProgressInt = null;
-defendProgressComplete = 0;
-progressInt = null;
-progressComplete = 0;
+   defense = () => {
+      this.defenseUsed = true;
+      return `Rogue has used Elude. He dodges the next attack`
+  }
+}
 
-// characters
+class FinalBoss extends Character {
+  spell = () => {
+      console.log('Siphon Soul')
+  }
 
-characters = [
-    {
-      name: "warrior",
-      resistance: ['steel'],
-      img: "../images/warrior back.png",
-      hp: {
-        current: 100,
-        total: 100
-      },
-      attacks: [
-        {
-          name: "Attack",
-          hp: randomNum(12,4),
-          avail: {
-            total: 30,
-            remaining: 30
-          }
-        },
-        {
-          name: "Spell",
-          hp: randomNum(60,45),
-          avail: {
-            total: 10,
-            remaining: 10
-          }
-        },
-        {
-          name: "Defense",
-          hp: randomNum(75,60),
-          avail: {
-            total: 5,
-            remaining: 5
-          }
-        },
-        {
-          name: "Physical",
-          hp: randomNum(160, 130),
-          avail: {
-            total: 2,
-            remaining: 2
-          }
-        }
-      ]
-    },
-    {
-        name: "queen",
-        resistance: ['ghost'],
-        img: "../images/Mage Face.jpeg",
-        hp: {
-          current: 80,
-          total: 80
-        },
-        attacks: [
-          {
-            name: "Attack",
-            hp: randomNum(12,4),
-            avail: {
-              total: 30,
-              remaining: 30
-            }
-          },
-          {
-            name: "Spell",
-            hp: randomNum(60,45),
-            avail: {
-              total: 10,
-              remaining: 10
-            }
-          },
-          {
-            name: "Defense",
-            hp: randomNum(75,60),
-            avail: {
-              total: 5,
-              remaining: 5
-            }
-          },
-          {
-            name: "Physical",
-            hp: randomNum(160, 130),
-            avail: {
-              total: 2,
-              remaining: 2
-            }
-          }
-        ]
-      },
-]
+  receiveDamage = (enemy, damage) => {
+      if (this.defenseUsed === true){
+          console.log(damage);
+          damage = damage/2;
+          this.health -= damage;
+          this.defenseUsed = false;
+      } else {
+          this.health -= damage;
+      }
+      if (this.health > 0) {
+        return `${this.name} has received ${damage} points of damage from ${enemy.name}. He now has a health of ${this.health}`;
+      } else {
+        return `${this.name} has died in act of combat.`;
+      }
+  };
+
+  defense = () => {
+      this.defenseUsed = true;
+      return `Final Boss has used Demoralize. He will take half damage in the next round`
+  }
+}
+
+class Grunt extends Character {
+  spell = () => {
+      console.log('Scream')
+  }
+
+  defense = () => {
+      this.health += 10;
+      return `'Grunt has used heal. His health is now ${this.health}`
+  }
+}
+
+class Dragon extends Character {
+  spell = () => {
+      console.log('Breathe Fire')
+  }
+  
+  receiveDamage = (enemy, damage) => {
+      if (this.defenseUsed === true){
+          enemy.health -= damage;
+          this.defenseUsed = false;
+      } else {
+          this.health -= damage;
+      }
+      if (this.health > 0) {
+        return `${this.name} has received ${damage} points of damage from ${enemy.name}. Now he has a health of ${this.health}`;
+      } else {
+        return `${this.name} has died in act of combat.`;
+      }
+  };
+
+  defense = () => {
+      this.defenseUsed = true;
+      return `Dragon has used Mirror Image. Enemy will take their own damage`
+  }
+}
+
+class IceWoman extends Character {
+  spell = () => {
+      console.log('Ice Bolt')
+  }
+
+  receiveDamage = (enemy, damage) => {
+      if (this.defenseUsed === true){
+          damage = 0;
+          this.health -= damage;
+          this.defenseUsed = false;
+      } else {
+          this.health -= damage;
+      }
+      if (this.health > 0) {
+        return `${this.name} has received ${damage} points of damage from ${enemy.name}. He now has a health of ${this.health}`;
+      } else {
+        return `${this.name} has died in act of combat.`;
+      }
+  };
+
+  defense = () => {
+    this.defenseUsed = true;
+    return `Ice Woman has used Freeze. She is stops incoming damage in the next round`
+  }
+} 
+
+let warrior = new Warrior('Warrior', 12, 6, 100, 2, 2);
+
+
+let mage = new Mage('Mage', 6, 12, 80, 0, 2);
+
+
+let rogue = new Rogue('Rogue', 9, 9, 90, 3, 2);
+
+
+let finalBoss = new FinalBoss('Final Boss', 25, 14, 60, 3, 2);
+
+
+let grunt = new Grunt('Grunt', 12, 5, 30, 1, 3);
+
+
+let dragon = new Dragon('Dragon', 9, 10, 30, 2, 2);
+
+
+let iceWoman = new IceWoman('Ice Woman', 5, 12, 25, 1 ,2);
--- game/map.js
+++ game/map.js
@@ -151,209 +151,25 @@
     
 /* ============================ CLASSES AND FUNCTIONALITIES FOR CHARACTERS ================================= */
 
+
 class Character {
-  constructor(name, strength, spellPower, health, spellCooldown, defenseCooldown) {
+  constructor(name, img, strength, health, x, y, sx, sy) {
+      this.img = img;
       this.name = name,
       this.strength = strength,
-      this.spellPower = spellPower,
-      this.spellCooldown = spellCooldown,
       this.health = health,
-      this.defenseCooldown = defenseCooldown,
-      this.defenseUsed = false
+      this.x = x,
+      this.y = y,
+      this.sx = sx,
+      this.sy = sy
   }
 
-  receiveDamage = (enemy, damage) => {
-
-      this.health -= damage;
-      if (this.health > 0) {
-        return `${this.name} has received ${damage} points of damage from ${enemy.name}. Now he has a health of ${this.health}`;
-      } else {
-        return `${this.name} has died in act of combat.`;
-      }
-  };
-
-  physicalAttack = (enemy) => {
-      return enemy.receiveDamage(this, Math.floor(Math.random() * this.strength));
-  }
-
-  spellAttack = (enemy) => {
-      return enemy.receiveDamage(this, Math.floor(Math.random() * this.strength)+1);
+  draw = () => {
+      //  STRUCTURE: context.drawImage(img, sx, sy, swidth, sheight, x, y, width, height);
+      // ctx.drawImage(this.img, 0, 0, 70, 65, this.x, this.y, canvas.width/ 13, canvas.height/ 13);
+      ctx.drawImage(this.img, this.sx, this.sy, 70, 65, this.x, this.y, canvas.width/ 13, canvas.height/ 13);
   }
 }
-
-class Warrior extends Character {
-
-  spell = () => {
-      console.log('Toss Axe');
-  }
-
-  receiveDamage = (enemy, damage) => {
-      if (this.defenseUsed === true){
-          this.health -= damage/2;
-          this.defenseUsed = false;
-      } else {
-          this.health -= damage;
-      }
-      if (this.health > 0) {
-        return `${this.name} has received ${damage} points of damage from ${enemy.name}. Now he has a health of ${this.health}`;
-      } else {
-        return `${this.name} has died in act of combat.`;
-      }
-  };
-
-  defense = () => {
-      this.defenseUsed = true;
-  }
-}
-
-class Mage extends Character {
-
-  spell = () => {
-      console.log('Magic Burst')
-  }
-
-  defense = () => {
-      this.health += 15;
-      return `'Mage has used Mend Wounds. Her health is now ${this.health}`
- }
-}
-
-class Rogue extends Character {
-
-  spell = () => {
-       console.log('Shadow Step')
-   }
-
-   receiveDamage = (enemy, damage) => {
-      if (this.defenseUsed === true){
-          damage = 0;
-          this.health -= damage;
-          this.defenseUsed = false;
-      } else {
-          this.health -= damage;
-      }
-      if (this.health > 0) {
-        return `${this.name} has received ${damage} points of damage from ${enemy.name}. He now has a health of ${this.health}`;
-      } else {
-        return `${this.name} has died in act of combat.`;
-      }
-  };
-
-
-   defense = () => {
-      this.defenseUsed = true;
-      return `Rogue has used Elude. He dodges the next attack`
-  }
-}
-
-class FinalBoss extends Character {
-  spell = () => {
-      console.log('Siphon Soul')
-  }
-
-  receiveDamage = (enemy, damage) => {
-      if (this.defenseUsed === true){
-          console.log(damage);
-          damage = damage/2;
-          this.health -= damage;
-          this.defenseUsed = false;
-      } else {
-          this.health -= damage;
-      }
-      if (this.health > 0) {
-        return `${this.name} has received ${damage} points of damage from ${enemy.name}. He now has a health of ${this.health}`;
-      } else {
-        return `${this.name} has died in act of combat.`;
-      }
-  };
-
-  defense = () => {
-      this.defenseUsed = true;
-      return `Final Boss has used Demoralize. He will take half damage in the next round`
-  }
-}
-
-class Grunt extends Character {
-  spell = () => {
-      console.log('Scream')
-  }
-
-  defense = () => {
-      this.health += 10;
-      return `'Grunt has used heal. His health is now ${this.health}`
-  }
-}
-
-class Dragon extends Character {
-  spell = () => {
-      console.log('Breathe Fire')
-  }
-  
-  receiveDamage = (enemy, damage) => {
-      if (this.defenseUsed === true){
-          enemy.health -= damage;
-          this.defenseUsed = false;
-      } else {
-          this.health -= damage;
-      }
-      if (this.health > 0) {
-        return `${this.name} has received ${damage} points of damage from ${enemy.name}. Now he has a health of ${this.health}`;
-      } else {
-        return `${this.name} has died in act of combat.`;
-      }
-  };
-
-  defense = () => {
-      this.defenseUsed = true;
-      return `Dragon has used Mirror Image. Enemy will take their own damage`
-  }
-}
-
-class IceWoman extends Character {
-  spell = () => {
-      console.log('Ice Bolt')
-  }
-
-  receiveDamage = (enemy, damage) => {
-      if (this.defenseUsed === true){
-          damage = 0;
-          this.health -= damage;
-          this.defenseUsed = false;
-      } else {
-          this.health -= damage;
-      }
-      if (this.health > 0) {
-        return `${this.name} has received ${damage} points of damage from ${enemy.name}. He now has a health of ${this.health}`;
-      } else {
-        return `${this.name} has died in act of combat.`;
-      }
-  };
-
-  defense = () => {
-    this.defenseUsed = true;
-    return `Ice Woman has used Freeze. She is stops incoming damage in the next round`
-  }
-} 
-
-let warrior = new Warrior('Warrior', 12, 6, 100, 2, 2);
-
-
-let mage = new Mage('Mage', 6, 12, 80, 0, 2);
-
-
-let rogue = new Rogue('Rogue', 9, 9, 90, 3, 2);
-
-
-let finalBoss = new FinalBoss('Final Boss', 25, 14, 60, 3, 2);
-
-
-let grunt = new Grunt('Grunt', 12, 5, 30, 1, 3);
-
-
-let dragon = new Dragon('Dragon', 9, 10, 30, 2, 2);
-
-
-let iceWoman = new IceWoman('Ice Woman', 5, 12, 25, 1 ,2);
 
 
 // warrior image
@@ -369,8 +185,8 @@ let story = new Image();
 story.src = "../images/storyCastle.jpeg";
 
 // INSTANCES OF CLASSES
-let newSkeleton = new Villain( 'Skeleton', character, 12, 100 , canvas.width / 1.7, canvas.height / 10, 50, 260);
-let newWarrior = new Warrior( 'Warrior', character, 12, 100 , 0, canvas.height / 1.6, 0, 0);
+let newSkeleton = new Character( 'Skeleton', character, 12, 100 , canvas.width / 1.7, canvas.height / 10, 50, 260);
+let newWarrior = new Character( 'Warrior', character, 12, 100 , 0, canvas.height / 1.6, 0, 0);
 
 /* ============================ ANIMATION ================================= */
 
