1f4a03d2a1f656b3d2a2bd3d2b51358b87a3da68
jose fraga <josemendezfraga@gmail.com>
2021-06-24 16:57:44 -0400

    story fixed

---
+++ story/story.js
@@ -0,0 +1,18 @@
+
+
+let counter = 0;
+window.onkeydown = function (e) {
+    console.log(e.key);
+    if (e.key === "Enter") {
+      counter++;
+      if (counter === 1) {
+       document.querySelector('p').innerText = "Everyone had hoped of finding that one hero that could defeat him."; 
+      }
+      if (counter === 2) {
+        document.querySelector('p').innerText = "This time you will be the hero they need... or not."
+      }
+      if (counter === 3) {
+        window.location.href = "../Assets/Maps/forest/forest.html";
+      }
+    }
+}
