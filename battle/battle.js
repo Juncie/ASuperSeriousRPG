d67753d3b6cb80d62defd07669fa1fc70edfdc4e
jose fraga <josemendezfraga@gmail.com>
2021-06-24 10:05:50 -0400

    Merge branch 'main' of https://github.com/GameProject-A-J-B/code-week-game-project

--- battle/battle.js
+++ battle/battle.js
@@ -1,3 +1,5 @@
+
+
 class Character {
     constructor(name, strength, spellPower, health, spellCooldown, defenseCooldown) {
         this.name = name,
@@ -10,7 +12,6 @@ class Character {
         this.defenseCooldownCounter = 0,
         this.spellCooldownCounter = 0
     }
-    
     // receiveDamage = (enemy, damage) => {
     //     this.health -= damage;
     //     if (this.health > 0) {
@@ -19,8 +20,6 @@ class Character {
     //       return `${this.name} has died in act of combat.`;
     //     }
     // };
-
-
     physicalAttack = (enemy) => {
         return enemy.receiveDamage(this, Math.floor(Math.random() * this.strength));
     }
@@ -31,20 +30,26 @@ class Character {
         }
     }
 }
+
 class Warrior extends Character {
   spell = () => {
       console.log('Toss Axe');
   }
   receiveDamage = (enemy, damage) => {
+
       if (this.defenseUsed === true){
+          let warrior = this;
           this.health -= damage/2;
+          setTimeout(function() { document.querySelector('.bar').style.width = warrior.health + "px";},3000) 
           this.defenseUsed = false;
       } else {
-          this.health -= damage;
+            let warrior = this;
+            setTimeout(function() {document.querySelector('.bar').style.width = warrior.health + "px"; },3000) 
+            this.health -= damage;
       }
       if (this.health > 0) {
         let warr = this;
-         setTimeout(function() {document.querySelector('p').innerText = `${warr.name} has received ${damage} point(s) of damage from ${enemy.name}. Now he has a health of ${warr.health}`;},3000) 
+         setTimeout(function() { document.querySelector('p').innerText = `${warr.name} has received ${damage} point(s) of damage from ${enemy.name}. Now he has a health of ${warr.health}`;},3000) 
         
           return `${this.name} has received ${damage} point(s) of damage from ${enemy.name}. Now he has a health of ${this.health}`;
       } else {        
@@ -124,9 +129,9 @@ class FinalBoss extends Character {
       }
   }
 }
-class Grunt extends Character {
+class Dragon extends Character {
   spell = () => {
-      console.log('Scream')
+      console.log('Breath Fire');
   }
   defense = (round) => {
       if (this.defenseCooldownCounter <= round || this.defenseCooldownCounter == 0){
@@ -137,19 +142,20 @@ class Grunt extends Character {
       }
   }
 }
-class Dragon extends Character {
+class Grunt extends Character {
   spell = () => {
-      console.log('Breathe Fire')
+    console.log('Scream')
   }
   receiveDamage = (enemy, damage) => {
-    // document.querySelector("#villian").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
       if (this.defenseUsed === true){
           enemy.health -= damage;
+          document.querySelector('.bar2').style.width = this.health + "px";
           this.defenseUsed = false;
           document.querySelector('p').innerText = `${enemy.name} has received ${damage} point(s) of damage from their own attack! They now have a health of ${enemy.health}`
           return `${enemy.name} has received ${damage} point(s) of damage from their own attack! They now have a health of ${enemy.health}`
       } else {
-          this.health -= damage;
+            this.health -= damage;
+            document.querySelector('.bar2').style.width = this.health + "px";
       }
       if (this.health > 0) {
         document.querySelector('p').innerText =`${this.name} has received ${damage} point(s) of damage from ${enemy.name}. Now he has a health of ${this.health}`
@@ -202,8 +208,8 @@ let warrior = new Warrior('Warrior', 12, 6, 130, 2, 2);
 //let mage = new Mage('Mage', 6, 12, 80, 0, 2);
 //let rogue = new Rogue('Rogue', 9, 9, 90, 3, 2);
 let finalBoss = new FinalBoss('Final Boss', 25, 14, 60, 3, 2);
-let grunt = new Grunt('Grunt', 12, 5, 30, 1, 3);
-let dragon = new Dragon('Dragon', 9, 10, 30, 2, 2);
+// let grunt = new Grunt('Grunt', 12, 5, 30, 1, 3);
+let grunt = new Grunt('Grunt', 9, 10, 40, 2, 2);
 //let iceWoman = new IceWoman('Ice Woman', 5, 12, 25, 1 ,2);
 function fight (hero,enemy) {
 let round = 0;
@@ -274,9 +280,6 @@ let round = 0;
 }
 
 let item = document.querySelectorAll('li')
-var audio = document.getElementsByTagName("audio")[0];
-
-
 for(let eachItem of item) {
 
 eachItem.addEventListener("mouseover", func, false);
@@ -284,8 +287,7 @@ eachItem.addEventListener("mouseout", func1, false);
 
 function func()
 {  
-   eachItem.setAttribute("style", "background-color:#000000;");
-   audio.play();
+   eachItem.setAttribute("style", "background-color:#fff237a1;")
 }
 
 function func1()
@@ -295,5 +297,4 @@ function func1()
 
 }
 
-window.onload = fight(warrior, dragon);
-
+window.onload = fight(warrior, grunt);
