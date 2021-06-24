d129c7b4d5cb6f16e52b0d216e4cf05ef42c01d0
jose fraga <josemendezfraga@gmail.com>
2021-06-24 15:23:44 -0400

    small changes

--- battle/battle.js
+++ battle/battle.js
@@ -226,7 +226,7 @@ class Lich extends Character {
 }
 
 
-let warrior = new Warrior('Warrior', 12, 6, 110, 2, 2);
+let warrior = new Warrior('Warrior', 12, 6, 140, 2, 2);
 let finalBoss = new FinalBoss('Final Boss', 25, 14, 80, 3, 2);
 let lich = new Lich('lich', 12, 5, 40, 1, 3);
 let dragon = new Dragon('Dragon', 9, 10, 50, 2, 2);
