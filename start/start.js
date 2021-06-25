ea439cee2c3205eaf28013ce7cf480aa6baa4f88
Brandon <BrandonMitchell5365@gmail.com>
2021-06-25 11:18:37 -0700

    story

--- start/start.js
+++ start/start.js
@@ -10,15 +10,21 @@ window.onkeydown = function (e) {
     if (e.key === "Enter") {
       counter++;
       if (counter === 1) {
-        document.querySelector('.content').innerHTML = "<p>A long time ago there was a monster that terrified the whole forest.</p>"; 
+        document.querySelector('.content').innerHTML = "<p>A long time ago, in a land about a 32 pixels away...</p>"; 
        }
       if (counter === 2) {
-       document.querySelector('.content').innerHTML = "<p>Everyone had hoped of finding that one hero that could defeat him.</p>"; 
+       document.querySelector('.content').innerHTML = "<p>Or was it 16-pixels? Maybe </p>"; 
       }
       if (counter === 3) {
-        document.querySelector('.content').innerHTML = "<p>This time you will be the hero they need... or not.</p>"; 
+        document.querySelector('.content').innerHTML = "<p>Doesn't matter, anyway, there was a noble Warrior who heard word of a village seeking protection from monsters</p>"; 
       }
       if (counter === 4) {
+        document.querySelector('.content').innerHTML = "<p>The Warrior, too afraid to fight monsters, left the village and went into the forest to hide instead</p>"; 
+      }
+      if (counter === 5) {
+        document.querySelector('.content').innerHTML = "<p>But little did he know...</p>"; 
+      }
+      if (counter === 6) {
         window.location.href = "../Assets/Maps/forest/forest.html";
       }
     }
